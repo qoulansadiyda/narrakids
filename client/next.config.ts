24ecab6597cfbaefecde566d339be70c3a04c294
Qoulan Sadiyda <qoulansadiyda22@gmail.com>
2026-04-01 19:23:03 +0700

    fix: next.js rewrites for bypass mixed content https blocked

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        // Menyembunyikan IPv4 asli di belakang Vercel agar tidak diblokir HTTPS browser
+        source: '/api/:path*',
+        destination: 'http://202.155.91.75/:path*'
+      },
+      {
+        // Mengalihkan koneksi real-time Socket.io
+        source: '/socket.io/:path*',
+        destination: 'http://202.155.91.75/socket.io/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
